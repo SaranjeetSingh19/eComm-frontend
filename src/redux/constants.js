b08e0e8cfe3202023bf1bc5d38e1b85ecca20cd1
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-14 17:59:28 +0530

    Deployment 0.13

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,7 +1,7 @@
 // import { server } from "../utils/config";
 
-// export const BASE_URL = server;
-export const BASE_URL = import.meta.env.VITE_API_URL
+export const BASE_URL = "";
+// export const BASE_URL = import.meta.env.VITE_API_URL;
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const config = {
-    plugins: [react()],
-  };
-
-  if (mode === 'development') {
-    config.server = {
-      proxy: {
-        "/api/": "http://localhost:5000",
-        "/uploads/": "http://localhost:5000",
-      },
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/": "https://ecomm-backend-4l7o.onrender.com",
+      "/uploads/": "https://ecomm-backend-4l7o.onrender.com",
+    }
   }
-
-  return config;
-});
+})
