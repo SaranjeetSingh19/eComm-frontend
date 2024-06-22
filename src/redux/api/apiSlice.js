89e380d3604af8e58c14d2500ea0ee14a6e25630
SaranjeetSingh19 <ss5259603@gmail.com>
2024-06-22 11:15:14 +0530

    Final

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,8 +1,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
-// import { BASE_URL } from "../constants";
-// import { server } from "../../utils/config";
+import { BASE_URL } from "../constants";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "https://ecomm-backend-2jri.onrender.com" });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
   reducerPath: "apiSlice",
--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,10 +1,9 @@
 import { server } from "../utils/config";
 
-// export const BASE_URL = server;
-export const BASE_URL = "";
+export const BASE_URL = server;
+// export const BASE_URL = "";
 export const USERS_URL = "/api/users";
 export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = "/api/products";
 export const UPLOAD_URL = "/api/upload";
 export const ORDERS_URL = "/api/orders";
-// export const PAYPAL_URL = "/api/config/paypal";
